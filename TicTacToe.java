69ae6270d250e8a543db8f6812648d47ff4c4cdd
Owen Lovett <olovett214@gmail.com>
2022-04-21 15:45:03 -0700

    added printBoard method

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,11 +3,19 @@ public class TicTacToe{
     public static void main(String[] args){
 
         char[][] board = {
-            {' ', '|', ' ', '|', ' '},
+            {'1', '|', '2', '|', '3'},
             {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '},
+            {'4', '|', '5', '|', '6'},
             {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '}
+            {'7', '|', '8', '|', '9'}
         };
+
+        printBoard(board);
+    }
+
+    private static void printBoard(char[][] board) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(board[i][0] + " " + board[i][1] + " " + board[i][2] + " " + board[i][3] + " " + board[i][4]);
+        }
     }
 }
