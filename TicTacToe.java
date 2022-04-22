4131bfe88234dbe55583ac993762b2437eed802b
Owen Lovett <olovett214@gmail.com>
2022-04-21 22:37:23 -0700

    added player1turn method

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,67 @@
+import java.util.Scanner;
+
 public class TicTacToe{
 
     public static void main(String[] args){
 
-        char[][] board = {
+        char[][] boardex = {
             {'1', '|', '2', '|', '3'},
             {'-', '+', '-', '+', '-'},
             {'4', '|', '5', '|', '6'},
             {'-', '+', '-', '+', '-'},
             {'7', '|', '8', '|', '9'}
         };
+
+        char[][] board = {
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}
+        };
+
+        Scanner k = new Scanner(System.in);
+
+        player1turn(board, k);
+
+    }
+
+    private static void player1turn(char[][] board, Scanner k) {
+        System.out.println("Where would you like to place your marker? (Please enter 1-9): ");
+
+        int player1input = k.nextInt();
+
+        switch (player1input){
+            case 1:
+                board[0][0] = 'X';
+                break;
+            case 2:
+                board[0][2] = 'X';
+                break;
+            case 3:
+                board[0][4] = 'X';
+                break;
+            case 4:
+                board[2][0] = 'X';
+                break;
+            case 5:
+                board[2][2] = 'X';
+                break;
+            case 6:
+                board[2][4] = 'X';
+                break;
+            case 7:
+                board[4][0] = 'X';
+                break;
+            case 8:
+                board[4][2] = 'X';
+                break;
+            case 9:
+                board[4][4] = 'X';
+                break;
+            default:
+                System.out.println("Error: Please only input 1-9.");
+        }
 
         printBoard(board);
     }
