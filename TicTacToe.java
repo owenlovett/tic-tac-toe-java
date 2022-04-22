7d71f48049d1f98e66fcefe94ceb73d75bb26717
Owen Lovett <olovett214@gmail.com>
2022-04-21 22:38:44 -0700

    added player2turn method

--- TicTacToe.java
+++ TicTacToe.java
@@ -66,6 +66,46 @@ public class TicTacToe{
         printBoard(board);
     }
 
+    private static void player2turn(char[][] board, Scanner k) {
+        System.out.println("Where would you like to place your marker? (Please enter 1-9): ");
+
+        int player2input = k.nextInt();
+
+        switch (player2input){
+            case 1:
+                board[0][0] = 'O';
+                break;
+            case 2:
+                board[0][2] = 'O';
+                break;
+            case 3:
+                board[0][4] = 'O';
+                break;
+            case 4:
+                board[2][0] = 'O';
+                break;
+            case 5:
+                board[2][2] = 'O';
+                break;
+            case 6:
+                board[2][4] = 'O';
+                break;
+            case 7:
+                board[4][0] = 'O';
+                break;
+            case 8:
+                board[4][2] = 'O';
+                break;
+            case 9:
+                board[4][4] = 'O';
+                break;
+            default:
+                System.out.println("Error: Please only input 1-9.");
+        }
+
+        printBoard(board);
+    }
+
     private static void printBoard(char[][] board) {
         for (int i = 0; i < 5; i++){
             System.out.println(board[i][0] + " " + board[i][1] + " " + board[i][2] + " " + board[i][3] + " " + board[i][4]);
